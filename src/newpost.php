7fe7f70b1f052048da8576116e8a36f540fddfef
arthurztt <artkikk1@gmail.com>
2025-12-31 09:38:37 -0300

    Alterações visuais no input

--- src/newpost.php
+++ src/newpost.php
@@ -28,7 +28,7 @@ class Post {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crie um Post</title>
-    <link rel="stylesheet" href="/css/newpost.css">
+    <link rel="stylesheet" href="/public/css/newpost.css">
 </head>
 <body>
     <form action="newpost.php" method="get">
