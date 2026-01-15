bf6a1a2c03beafcf6ca1c382cd13f72f5a90281d
arthurztt <artkikk1@gmail.com>
2026-01-15 12:04:55 -0300

    Ajustando o card de post e suas informações, ajustando alguns caminhos

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/public/css/style.css">
     <title>Feed Principal</title>
 </head>
 <body>
@@ -17,14 +17,14 @@
     <main>
         <section class="mainfeed">
             <div class="title">
-            <h1>As <span class="italic">paradas&#9835;</span> que os outros estão Compartilhando !</h1>
+            <!-- <h1>As <span class="italic">paradas&#9835;</span> que os outros estão Compartilhando !</h1> -->
             </div>
             <div class="posts">
                 <!-- Exemplo de Post -->
                 <div class="post createpost">
                     <h2>Que tal compartilhar uma música?</h2>
                     <p>Use o link do Spotify para compartilhar suas músicas favoritas com a comunidade!</p>
-                    <form action="/app/newpost.php" method="get">
+                    <form action="/src/newpost.php" method="get">
                     <button class="newpost">
                             <input type="submit" value="Compartilhar Nova Música"> 
                             <span>+</span>
@@ -34,9 +34,10 @@
                 <div class="post">
                     <div class="musicinfo">
                         <h3 class="music-title">Trem de Doido</h3>
-                        <h3 class="album-name">Clube da Esquina</>
-                        <p class="artist">Lô Borges, Milton Nascimento</p>
-                        <p class="music-duration">3:58</p>
+                        <p>▸</p>
+                        <h3 class="artist">Lô Borges, Milton Nascimento</h3>
+                        <img src="/public/img/stopwatch.svg" class="item-aside">
+                        <p class="music-duration extra-info"> 3min 48s</p>
                     </div>
                     <div class="infobox">
                         <p class="description">Boa música, solo de guitarra insano.</p>
@@ -47,7 +48,7 @@
             <div class="profile">
                 <hr>
                 <h2>Meu Perfil</h2>
-                <img src="/img/Marilyn-Monroe.webp"profileimg" alt="Imagem de Perfil">
+                <img src="" class="profileimg" alt="Imagem de Perfil">
                 <p class="username">arthurztt</p>
                 <p class="profile-description">Perfil para compartilhar músicas que escuto no momento !</p>
                 <p class="posts">0 Posts</p>
