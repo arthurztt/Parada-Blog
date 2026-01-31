776999b2912ca97cad619145ef16a509494a1879
arthurztt <artkikk1@gmail.com>
2026-01-31 19:54:30 -0300

    Nova estilização do card de post, e ajustando o caminhdo das imagens após reajuste

--- public/index.php
+++ public/index.php
@@ -17,10 +17,9 @@
     <main>
         <section class="mainfeed">
             <div class="title">
-            <!-- <h1>As <span class="italic">paradas&#9835;</span> que os outros estão Compartilhando !</h1> -->
+            <h1>As <span class="italic">paradas&#9835;</span> que os outros estão Compartilhando !</h1>
             </div>
             <div class="posts">
-                <!-- Exemplo de Post -->
                 <div class="post createpost">
                     <h2>Que tal compartilhar uma música?</h2>
                     <p>Use o link do Spotify para compartilhar suas músicas favoritas com a comunidade!</p>
@@ -36,11 +35,18 @@
                         <h3 class="music-title">Trem de Doido</h3>
                         <p>▸</p>
                         <h3 class="artist">Lô Borges, Milton Nascimento</h3>
-                        <img src="/public/img/stopwatch.svg" class="item-aside">
+                        <img src="/public/img/icons/stopwatch.svg" class="item-aside">
                         <p class="music-duration extra-info"> 3min 48s</p>
                     </div>
                     <div class="infobox">
                         <p class="description">Boa música, solo de guitarra insano.</p>
+                    </div>
+                    <div class="user-info">
+                        <p class="post-creator">arthurztt</p>
+                        <?php 
+                            $now = new DateTime('America/Fortaleza');
+                            echo $now->format('d/m/Y H:i');
+                        ?>
                     </div>
             </div>
         </section>
@@ -48,13 +54,13 @@
             <div class="profile">
                 <hr>
                 <h2>Meu Perfil</h2>
-                <img src="" class="profileimg" alt="Imagem de Perfil">
+                <img src="/public/img/person-circle.svg" class="profileimg" alt="Imagem de Perfil">
                 <p class="username">arthurztt</p>
                 <p class="profile-description">Perfil para compartilhar músicas que escuto no momento !</p>
                 <p class="posts">0 Posts</p>
             </div>
             <div class="external-links">
-                <a href="https://open.spotify.com/user/31z6kcwwmaeloimnht6uf67phqsm?si=57dfc6b5e579478b" class="external-app-account"><img class="external-link-img" src="/img/box-arrow-up-right.svg">Perfil Musical</a>
+                <a href="https://open.spotify.com/user/31z6kcwwmaeloimnht6uf67phqsm?si=57dfc6b5e579478b" class="external-app-account"><img class="external-link-img" src="/public/img/icons/box-arrow-up-right.svg">Perfil Musical</a>
             </div>
         </section>
     </main>
